d234261c9e953b766a300abdc9240f2b6eda31f0
SKKUGoon <54009920+SKKUGoon@users.noreply.github.com>
2022-12-03 17:17:07 +0900

    add flag to employ deploy option

--- exec_band.py
+++ exec_band.py
@@ -3,10 +3,18 @@ from cex.binance_future import BinanceFutureX
 from cex.domestic import UpbitX
 from sig.sig0 import StrategyIEXA
 from sig.sig0_gen import gen_band_iexa
+
 import time
+import argparse
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    # -env ENVIRONMENT 
+    parser.add_argument("-env", "--environment", help="Type of Environment, Dev or Deploy")
+
+    args = parser.parse_args()
+
     cm = CexFactoryX()
     binance_x = BinanceFutureX()
     upbit_x = UpbitX()
@@ -20,6 +28,7 @@ if __name__ == "__main__":
         a,
         exchange_long=upbit_x,
         exchange_short=binance_x,
+        env=args.environment,
     )
 
     
