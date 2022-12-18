84c08455651959a418f27823d16e97cc6cf56548
SKKUGoon <54009920+SKKUGoon@users.noreply.github.com>
2022-12-18 20:20:17 +0900

    delete tpsl order

--- cex/cex_factory_trade.py
+++ cex/cex_factory_trade.py
@@ -12,16 +12,11 @@ class CexManagerT(ABC):
         pass
 
     @abstractmethod
-    def order_buy(self, buy: dict):
+    def order_buy(self, buy: dict, leverage: int):
         pass
 
     @abstractmethod
     def order_sell(self, sell: dict):
-        pass
-
-    @abstractmethod
-    def order_tpsl(self, take_profit: float, stop_loss:float):
-        # TPSL: Take Profit Stop Loss
         pass
 
     @abstractmethod
@@ -34,20 +29,14 @@ class CexFactoryT:
         return exchange.connection()
 
     # Trader methods
-    def get_balance(self, exchange: CexManagerT):
-        return exchange.balance()
-
-    def exec_order_sell(self, exchange: CexManagerT):
-        return exchange.order_sell()
+    def get_balance(self, exchange: CexManagerT, key: str):
+        return exchange.balance(key.upper())
 
     def exec_order_buy(self, exchange: CexManagerT):
         return exchange.order_buy()
 
-    def app_order_tpsl(self, take_profit: float, stop_loss:float, exchange:CexManagerT):
-        return exchange.order_tpsl(
-            take_profit=take_profit,
-            stop_loss=stop_loss,
-        )
-
+    def exec_order_sell(self, exchange: CexManagerT):
+        return exchange.order_sell()
+        
     def report_trade_result(self, exchange: CexManagerT):
         return exchange
