9733b45731d4ee10d2c05af141d5f2b3e41e870e
SKKUGoon <54009920+SKKUGoon@users.noreply.github.com>
2022-12-27 02:29:56 +0900

    create print_*color* functions

--- utility/coloring.py
+++ utility/coloring.py
@@ -8,3 +8,25 @@ class PrettyColors:
     ENDC = '\033[0m'
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
+    
+    def print_bold(self, val):
+        print(self.BOLD + val + self.ENDC, flush=True)
+    
+    def print_underline(self, val):
+        print(self.UNDERLINE + val + self.ENDC, flush=True)
+    
+    def print_ok_blue(self, val):
+        print(self.OKBLUE + val + self.ENDC, flush=True)
+    
+    def print_ok_green(self, val):
+        print(self.OKGREEN + val + self.ENDC, flush=True)
+
+    def print_ok_cyan(self, val):
+        print(self.OKCYAN + val + self.ENDC, flush=True)
+
+    def print_warning(self, val):
+        print(self.WARNING + val + self.ENDC, flush=True)
+    
+    def print_fail(self, val):
+        print(self.FAIL + val + self.ENDC, flush=True)
+
